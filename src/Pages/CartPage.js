1cfb342971b215eec8572efc5a6f859a2fff5b0a
edwardash <edwardash00@gmail.com>
2023-03-16 15:46:07 +0530

    cost calculation done

--- src/Pages/CartPage.js
+++ src/Pages/CartPage.js
@@ -7,37 +7,59 @@ const CartPage = () => {
 
 
 
-
+    let fr = localStorage.getItem("freecart");
     const [data, setdata] = useState();
 
     const [cartList, setcartList] = useState();
+    const [freeItemsList, setfreeItemsList] = useState(fr ? JSON.parse(fr) : undefined);
+
+    const [totalCost, setTotalCost] = useState();
+    const [totalDiscount, setTotalDiscount] = useState();
 
 
     useEffect(() => {
-
-
-
-
-
-        // fetchAllData();
-
         let cartData = fetchCartItems();
-
-
-
     }, []);
 
 
 
+
+
+    useEffect(() => {
+        // let cartData = fetchCartItems();
+        if (cartList != undefined && cartList != null && cartList.length > 0) {
+            let total = 0;
+            cartList.map((item) => {
+                total += ((parseFloat(item.price.slice(1))) * item.quantity);
+            })
+
+            setTotalCost(total);
+        }
+    }, [cartList]);
+
+
+
+    useEffect(() => {
+        // let cartData = fetchCartItems();
+        if (freeItemsList != undefined && freeItemsList != null && freeItemsList.length > 0) {
+            let total = 0;
+            freeItemsList.map((item) => {
+                total += (parseFloat(item.price.slice(1)));
+            })
+
+            setTotalDiscount(total);
+        }
+    }, [freeItemsList]);
+
+
+
+
+
     const fetchCartItems = () => {
-
         let data = JSON.parse(localStorage.getItem("cart"));
-
         if (data != null && data != undefined && data.length > 0) {
             setcartList(data);
         }
-
-
     }
 
 
@@ -53,7 +75,7 @@ const CartPage = () => {
     }
 
     useEffect(() => {
-
+        // localStorage.setItem("cart", JSON.stringify(cartList))
     }, [cartList])
 
 
@@ -66,6 +88,9 @@ const CartPage = () => {
                     console.log("quantity 1");
                     // deleteThisItem(item.id);
                 } else {
+                    if ((id == 642 && item.quantity == 6) || (id == 532 && item.quantity == 3)) {
+                        deleteThisItemFree(id);
+                    }
                     item.quantity--;
                 }
             }
@@ -73,8 +98,23 @@ const CartPage = () => {
         })
 
         setcartList(newList);
+        localStorage.setItem("cart", JSON.stringify(newList))
+
+    }
 
 
+    const deleteThisItemFree = (id) => {
+        console.log("delete called");
+
+        if (freeItemsList) {
+            let newList = freeItemsList.filter((item) => {
+                return item.id != id;
+            });
+
+            console.log(newList);
+            localStorage.setItem("freecart", JSON.stringify(newList))
+            setfreeItemsList(newList);
+        }
     }
 
 
@@ -83,10 +123,20 @@ const CartPage = () => {
 
         let newList = cartList.map((item) => {
             if (item.id == id) {
+
+                // if (id == 642)
+
+
                 if (available == item.quantity) {
 
                 } else {
+                    if ((id == 642 && item.quantity == 5) || (id == 532 && item.quantity == 2)) {
+                        addFree(item);
+                    }
                     item.quantity++;
+                    // }
+                    // }
+
                 }
             }
             return item;
@@ -94,8 +144,48 @@ const CartPage = () => {
 
         setcartList(newList);
 
+        localStorage.setItem("cart", JSON.stringify(newList))
 
     }
+
+
+
+    const addFree = (item) => {
+
+        console.log("add free called");
+
+
+        let freecart = localStorage.getItem("freecart");
+        if (freecart == null || freecart == undefined) {
+            localStorage.setItem("freecart", JSON.stringify([item]));
+            setfreeItemsList([item]);
+        }
+        else {
+            let narr = JSON.parse(freecart);
+            if (checkExist(item.id, narr)) {
+
+            } else {
+                narr.push(item);
+                localStorage.setItem("freecart", JSON.stringify(narr));
+                setfreeItemsList(narr);
+            }
+
+        }
+
+    }
+
+
+    const checkExist = (id, arr) => {
+        let flag = false;
+        arr.map((item) => {
+            if (item.id == id) {
+                flag = true;
+            }
+        });
+        return flag;
+    }
+
+
 
 
 
@@ -126,7 +216,7 @@ const CartPage = () => {
                         <p>{item.price}</p>
 
 
-                        <span>Cross Icon</span>
+                        <span onClick={() => deleteThisItem(item.id)}>Cross Icon</span>
 
                     </div>
 
@@ -139,10 +229,47 @@ const CartPage = () => {
 
             }
 
+            FREE ITEMS:
+
+
+            {
+                freeItemsList && freeItemsList.map((item) => (
 
 
 
+                    <div className={styles.brdrs} key={item.id}>
 
+                        <img className={styles.minusIcon} alt="" src={item.img} />
+                        <p>  {item.name}</p>
+
+
+                        {/* <p>{item.description}</p> */}
+
+                        <p>1</p>
+                        {/* <p><span onClick={() => minus(item.id, item.available)} >-</span>  {item.quantity}   <span onClick={() => add(item.id, item.available)}>+</span></p> */}
+
+
+                        {/* <p>{item.available < 10 ? `Only ${item.available} left` : "Available"}</p> */}
+
+
+
+                        {/* <p>{item.price}</p> */}
+
+
+                        {/* <span onClick={() => deleteThisItem(item.id)}>Cross Icon</span> */}
+
+                    </div>
+
+
+                ))
+
+
+            }
+
+
+            <p>{totalCost}</p>
+
+            <p>{totalDiscount}</p>
 
         </>
     )

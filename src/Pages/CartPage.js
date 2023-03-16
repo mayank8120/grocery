cd3994915588f83de68e66c5c06c3a998f42e24f
edwardash <edwardash00@gmail.com>
2023-03-17 00:06:58 +0530

    Comments removed

--- src/Pages/CartPage.js
+++ src/Pages/CartPage.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
-import axios from 'axios';
-import styles from '../App.module.css';
 import Minus from '../img/Minus.png';
 import Plus from '../img/Plus.png';
 import Cross from '../img/crossicon.png';
@@ -10,6 +8,7 @@ import Cross from '../img/crossicon.png';
 import Avatar from '../img/avatar.png';
 import Cart from '../img/carticon.png';
 import hearticon from '../img/hearticon.png';
+import { useHistory } from 'react-router-dom';
 
 
 
@@ -17,7 +16,7 @@ import hearticon from '../img/hearticon.png';
 const CartPage = () => {
 
 
-
+    let history = useHistory();
 
     let fr = localStorage.getItem("freecart");
     const [data, setdata] = useState();
@@ -38,7 +37,6 @@ const CartPage = () => {
 
 
     useEffect(() => {
-        // let cartData = fetchCartItems();
         if (cartList != undefined && cartList != null && cartList.length > 0) {
             let total = 0;
             cartList.map((item) => {
@@ -52,7 +50,6 @@ const CartPage = () => {
 
 
     useEffect(() => {
-        // let cartData = fetchCartItems();
         if (freeItemsList != undefined && freeItemsList != null && freeItemsList.length > 0) {
             let total = 0;
             freeItemsList.map((item) => {
@@ -76,12 +73,10 @@ const CartPage = () => {
 
 
     const deleteThisItem = (id) => {
-        console.log("delete called");
         let newList = cartList.filter((item) => {
             return item.id != id;
         });
 
-        console.log(newList);
         localStorage.setItem("cart", JSON.stringify(newList));
         if (id == 642) {
             deleteThisItemFree(642);
@@ -94,7 +89,6 @@ const CartPage = () => {
     }
 
     useEffect(() => {
-        // localStorage.setItem("cart", JSON.stringify(cartList))
     }, [cartList])
 
 
@@ -104,8 +98,6 @@ const CartPage = () => {
         let newList = cartList.map((item) => {
             if (item.id == id) {
                 if (item.quantity <= 1) {
-                    console.log("quantity 1");
-                    // deleteThisItem(item.id);
                 } else {
                     if ((id == 642 && item.quantity == 6) || (id == 532 && item.quantity == 3)) {
                         if ((id == 532 && item.quantity == 3)) {
@@ -113,9 +105,6 @@ const CartPage = () => {
                         } else {
                             deleteThisItemFree(id);
                         }
-
-
-
                     }
                     item.quantity--;
                 }
@@ -130,14 +119,10 @@ const CartPage = () => {
 
 
     const deleteThisItemFree = (id) => {
-        console.log("delete called");
-
         if (freeItemsList) {
             let newList = freeItemsList.filter((item) => {
                 return item.id != id;
             });
-
-            console.log(newList);
             localStorage.setItem("freecart", JSON.stringify(newList))
             setfreeItemsList(newList);
         }
@@ -145,17 +130,11 @@ const CartPage = () => {
 
 
     const add = (id, available) => {
-
-
         let newList = cartList.map((item) => {
             if (item.id == id) {
                 if (available == item.quantity) {
-
                 } else {
                     if ((id == 642 && item.quantity == 5) || (id == 532 && item.quantity == 2)) {
-
-
-
                         let it = {
                             "id": 641,
                             "type": "drinks",
@@ -172,10 +151,6 @@ const CartPage = () => {
                         else {
                             addFree(item);
                         }
-
-
-
-                        // addFree(item);
                     }
                     item.quantity++;
                 }
@@ -192,10 +167,6 @@ const CartPage = () => {
 
 
     const addFree = (item) => {
-
-        console.log("add free called");
-
-
         let freecart = localStorage.getItem("freecart");
         if (freecart == null || freecart == undefined) {
             localStorage.setItem("freecart", JSON.stringify([item]));
@@ -210,9 +181,7 @@ const CartPage = () => {
                 localStorage.setItem("freecart", JSON.stringify(narr));
                 setfreeItemsList(narr);
             }
-
         }
-
     }
 
 
@@ -236,7 +205,7 @@ const CartPage = () => {
             <div className='w-[80%] mx-auto'>
 
                 <div className='flex justify-between items-center w-full h-full mt-[5%] mb-[3%]'>
-                    <div className='w-[20%] flex justify-start'>
+                    <div className='w-[20%] flex justify-start' onClick={() => history.push('/')}>
                         GROCERIES
                     </div>
                     <div className='w-[40%]'>
@@ -273,7 +242,6 @@ const CartPage = () => {
                                     <div className="flex flex-col justify-between ml-4 flex-grow">
                                         <span className="font-bold text-sm">{item.name}</span>
                                         <span className="text-red-500 text-xs">product ID : {item.id}</span>
-                                        {/* <a href="#" className="font-semibold hover:text-red-500 text-gray-500 text-xs">Remove</a> */}
                                     </div>
                                 </div>
                                 <div className='flex justify-center items-center flex-col w-1/5'>
@@ -297,31 +265,6 @@ const CartPage = () => {
                                 </div>
                             </div>
                         </div>
-
-
-                        // <div className={styles.brdrs} key={item.id}>
-
-                        //     <img className={styles.minusIcon} alt="" src={item.img} />
-                        //     <p>  {item.name}</p>
-
-                        //     <p><span onClick={() => minus(item.id, item.available)} >-</span>  {item.quantity}   <span onClick={() => add(item.id, item.available)}>+</span></p>
-
-
-                        //     <p>{item.available < 10 ? `Only ${item.available} left` : "Available"}</p>
-
-
-
-                        //     <p>{item.price}</p>
-
-
-                        //     <span onClick={() => deleteThisItem(item.id)}>Cross Icon</span>
-
-                        // </div>
-
-
-
-
-
                     ))
                 }
             </div>
@@ -330,8 +273,6 @@ const CartPage = () => {
                 <div className='ml-3'>FREE ITEMS:</div>
             </div>
             }
-
-
 
             {
                 freeItemsList && freeItemsList.map((item) => (
@@ -345,7 +286,6 @@ const CartPage = () => {
                                 <div className="flex flex-col justify-between ml-4 flex-grow">
                                     <span className="font-bold text-sm">{item.name}</span>
                                     <span className="text-red-500 text-xs">product ID : {item.id}</span>
-                                    {/* <a href="#" className="font-semibold hover:text-red-500 text-gray-500 text-xs">Remove</a> */}
                                 </div>
                             </div>
                             <div className='flex justify-center items-center flex-col w-1/5'>
@@ -354,66 +294,11 @@ const CartPage = () => {
                         </div>
                     </div>
 
-                    // <div className='w-[80%] mx-auto'>
-                    //     <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
-                    //         <div className="flex w-2/5">
-                    //             <div className="w-20">
-                    //                 <img className="h-24" src={item.img} alt="" />
-                    //             </div>
-                    //             <div className="flex flex-col justify-between ml-4 flex-grow">
-                    //                 <span className="font-bold text-sm">{item.name}</span>
-                    //                 <span className="text-red-500 text-xs">product ID : {item.id}</span>
-                    //             </div>
-                    //         </div>
-                    //         <div className='flex justify-center items-center flex-col'>
-                    //             <div className="flex justify-center w-1/5">
-                    //                 <img src={Minus} alt='minus' />
-
-                    //                 <input className="mx-2 border text-center w-8" type="text" value="1" />
-
-                    //                 <img src={Plus} alt='plus' />
-
-                    //             </div>
-                    //             <p>{item.available < 10 ? `Only ${item.available} left` : "Available"}</p>
-                    //         </div>
-                    //         <span className="text-center w-1/5 font-semibold text-sm">{item.price}</span>
-                    //         <span onClick={() => deleteThisItem(item.id)} className="text-center w-1/5 font-semibold text-sm">
-                    //             <img src={Cross} alt='cross' />
-                    //         </span>
-                    //     </div>
-                    // </div>
-
-                    // <div className={styles.brdrs} key={item.id}>
-
-                    //     <img className={styles.minusIcon} alt="" src={item.img} />
-                    //     <p>  {item.name}</p>
-
-
-                    //     {/* <p>{item.description}</p> */}
-
-                    //     <p>1</p>
-                    //     {/* <p><span onClick={() => minus(item.id, item.available)} >-</span>  {item.quantity}   <span onClick={() => add(item.id, item.available)}>+</span></p> */}
-
-
-                    //     {/* <p>{item.available < 10 ? `Only ${item.available} left` : "Available"}</p> */}
-
-
-
-                    //     {/* <p>{item.price}</p> */}
-
-
-                    //     {/* <span onClick={() => deleteThisItem(item.id)}>Cross Icon</span> */}
-
-                    // </div>
-
 
                 ))
             }
 
 
-            {/* <p>{totalCost}</p>
-
-            <p>{totalDiscount}</p> */}
 
             <div className='w-[80%] mx-auto mt-[5%] mb-[5%]'>
                 <div className='border-black border-t-2 h-[5rem] flex justify-center items-center'>
--- src/Pages/ListPage.js
+++ src/Pages/ListPage.js
@@ -1,32 +1,15 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import styles from '../App.module.css'
 import { useHistory } from "react-router-dom";
-// import Minus from './img/Minus.png';
-// import Plus from './img/Plus.png';
-
-// import Banana from './img/Banana.png';
-// import Avatar from './img/avatar.png';
-// import Cart from './img/carticon.png';
-// import Cross from './img/crossicon.png';
-// import filtericon from './img/filtericon.png';
-
-// import hearticon from './img/hearticon.png';
-
-// // import hearticon from './img/hearticon.png';
-// import peach from './img/peach.png';
-// // import hearticon from './img/hearticon.png';
-// // import hearticon from './img/hearticon.png';
-
 import Avatar from '../img/avatar.png';
 import Cart from '../img/carticon.png';
 import hearticon from '../img/hearticon.png';
 import filtericon from '../img/filtericon.png';
 
 
-
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const ListPage = () => {
@@ -34,16 +17,11 @@ const ListPage = () => {
     let history = useHistory();
 
     const [data, setdata] = useState();
-
-
-
-    // function handleClick() {
-    //   history.push("/home");
-    // }
+    const [filter, setFilter] = useState();
+    const [searchTerm, setSearchTerm] = useState("");
 
 
     useEffect(() => {
-
         var config = {
             method: 'get',
             maxBodyLength: Infinity,
@@ -53,9 +31,7 @@ const ListPage = () => {
 
         axios(config)
             .then(function (response) {
-
                 setdata(response.data);
-                console.log((response.data));
             })
             .catch(function (error) {
                 console.log(error);
@@ -64,18 +40,11 @@ const ListPage = () => {
     }, [])
 
 
-    const [filter, setFilter] = useState();
 
 
-
-
-    console.log(filter);
 
 
     function addToCart(comingData) {
-
-
-
         let local = localStorage.getItem("cart");
         let parse = JSON.parse(local);
         if (parse != null && parse != undefined && parse.length > 0) {
@@ -85,19 +54,16 @@ const ListPage = () => {
                 parse.push(data);
                 localStorage.setItem("cart", JSON.stringify(parse));
             } else {
-                console.log("exist");
             }
         } else {
             let data = comingData;
             data.quantity = 1;
-
-
             let arr = [data];
             let JSO = JSON.stringify(arr);
             localStorage.setItem("cart", JSO);
-            console.log("no element");
         }
 
+        toast(`${comingData.name} added to cart!`);
 
     }
 
@@ -105,15 +71,12 @@ const ListPage = () => {
 
     function findInArray(check, arr) {
         let flag = false;
-
         arr.map((item) => {
             if (item.id == check) {
                 flag = true;
             }
         });
-
         return flag;
-
     }
 
 
@@ -125,11 +88,11 @@ const ListPage = () => {
             <div className='w-[80%] mx-auto'>
 
                 <div className='flex justify-between items-center w-full h-full mt-[5%] mb-[3%]'>
-                    <div className='w-[20%] flex justify-start'>
+                    <div className='w-[20%] flex justify-start' onClick={() => history.push('/')}>
                         GROCERIES
                     </div>
                     <div className='w-[40%]'>
-                        <input type="text" className='w-full border border-2 rounded-full h-10' placeholder='  SEARCH' />
+                        <input type="text" onChange={(e) => setSearchTerm(e.target.value)} className='w-full border border-2 rounded-full h-10' placeholder='  SEARCH' />
                     </div>
                     <div className='w-[20%] flex items-center justify-end'>
                         <span className='mr-2 w-[51px] h-[49px]'>
@@ -145,10 +108,10 @@ const ListPage = () => {
                 </div>
 
                 <div className='flex justify-start'>
-                    <span className='rounded-full cursor-pointer mr-2 border border-2 py-1 px-4 m-2' onClick={() => setFilter()}>All Items </span>
-                    <span className='rounded-full cursor-pointer mr-2 border border-2 py-1 px-4 m-2' onClick={() => setFilter("drinks")}>Drinks</span>
-                    <span className='rounded-full cursor-pointer mr-2 border border-2 py-1 px-4 m-2' onClick={() => setFilter("fruit")}> Fruits </span>
-                    <span className='rounded-full cursor-pointer mr-2 border border-2 py-1 px-4 m-2' onClick={() => setFilter("bakery")}>Bakery </span>
+                    <span className={`${filter == null || filter == undefined || filter == "" ? "bg-gray-300" : ""} rounded-full cursor-pointer mr-2 border border-2 py-1 px-4 m-2`} onClick={() => setFilter()}>All Items </span>
+                    <span className={`${filter == "drinks" ? "bg-gray-300" : ""} rounded-full cursor-pointer mr-2 border border-2 py-1 px-4 m-2`} onClick={() => setFilter("drinks")}>Drinks</span>
+                    <span className={`${filter == "fruit" ? "bg-gray-300" : ""} rounded-full cursor-pointer mr-2 border border-2 py-1 px-4 m-2`} onClick={() => setFilter("fruit")}> Fruits </span>
+                    <span className={`${filter == "bakery" ? "bg-gray-300" : ""} rounded-full cursor-pointer mr-2 border border-2 py-1 px-4 m-2`} onClick={() => setFilter("bakery")}>Bakery </span>
                 </div>
 
                 <div className='flex justify-start'>
@@ -169,71 +132,64 @@ const ListPage = () => {
                             return true;
                         }
                     }
-                    ).map(
+                    )
 
-                        (item) =>
-                            <div className='flex shadow-lg rounded-lg justify-center p-4 m-3 h-full w-[518px]'>
-                                <div className='h-full w-[286px] flex justify-center items-center'
-                                >
-                                    <img className='w-[200px] h-[200px]' alt="" src={item.img} />
-                                </div>
+                        .filter((item) => {
 
-                                <div className='flex justify-around flex-col w-[286px]'>
-                                    <div>
-                                        <div>{item.name}</div>
-                                        <div>{item.description.slice(0, 73)}</div>
+                            //                             const searchQuery = search.toLowerCase();
+                            //   const itemName = item.name.toLowerCase();
+                            //   const itemDesc = item.description.toLowerCase();
+                            //   return itemName.includes(searchQuery) || itemDesc.includes(searchQuery);
+
+
+                            if (searchTerm == "") {
+                                return true;
+                            } else if (item.name.toLowerCase().includes(searchTerm.toLowerCase()) || (item.description.toLowerCase().includes(searchTerm.toLowerCase()))) {
+                                return true;
+                            } else {
+                                return false;
+                            }
+                        })
+
+
+
+                        .map(
+
+                            (item) =>
+                                <div className='flex shadow-lg rounded-lg justify-center p-4 m-3 h-full w-[518px]'>
+                                    <div className='h-full w-[286px] flex justify-center items-center'
+                                    >
+                                        <img className='w-[200px] h-[200px]' alt="" src={item.img} />
                                     </div>
-                                    <div className='flex justify-start'>
-                                        <button className='ml-2 rounded-full bg-orange-500 w-[40%]'>{item.available < 10 ? `Only ${item.available} left` : "Available"}</button>
-                                    </div>
-                                    <div className='flex justify-between items-center'>
-                                        <div>{item.price}</div>
-                                        <div className='flex justify-center'>
-                                            <div className='mr-2 cursor-pointer'>
-                                                <img src={Cart} alt='' className='h-[27px] w-[27px]' onClick={() => addToCart(item)} />
-                                            </div>
-                                            <div className='mr-2'>
-                                                <img src={hearticon} alt='' className='h-[27px] w-[27px]' />
+
+                                    <div className='flex justify-around flex-col w-[286px]'>
+                                        <div>
+                                            <div>{item.name}</div>
+                                            <div>{item.description.slice(0, 73)}</div>
+                                        </div>
+                                        <div className='flex justify-start'>
+                                            <button className='ml-2 rounded-full bg-orange-500 w-[40%]'>{item.available < 10 ? `Only ${item.available} left` : "Available"}</button>
+                                        </div>
+                                        <div className='flex justify-between items-center'>
+                                            <div>{item.price}</div>
+                                            <div className='flex justify-center'>
+                                                <div className='mr-2 cursor-pointer'>
+                                                    <img src={Cart} alt='' className='h-[27px] w-[27px]' onClick={() => addToCart(item)} />
+                                                </div>
+                                                <div className='mr-2'>
+                                                    <img src={hearticon} alt='' className='h-[27px] w-[27px]' />
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-
-                        // <div className='' key={item.id} onClick={() => addToCart(item)}>
-
-                        //     <img className={styles.minusIcon} alt="" src={item.img} />
-                        //     <p>  {item.name}</p>
-
-
-                        //     <p>{item.description}</p>
-
-
-                        //     <p>{item.available < 10 ? `Only ${item.available} left` : "Available"}</p>
-
-
-
-                        //     <p>{item.price}</p>
-
-
-                        //     <p>cart icon</p>
-
-                        //     <p>heart icon</p>
-
-                        // </div>
-
-                    )
-
-
+                        )
                     }
 
                 </div>
-
-
             </div>
 
-
-
+            <ToastContainer />
         </>
     )
 }
